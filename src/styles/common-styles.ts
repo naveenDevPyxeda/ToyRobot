c65701cd252a015217536741eb458fcc4f74c2e6
DESKTOP-FLKJGT3\DELL <naveen.atukorala@pyxeda.ai>
2025-01-03 11:39:35 +1030

    add(common styling)

---
+++ src/styles/common-styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background-color: white;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  padding: 20px;
+  border-radius: 20px;
+  width: 250px;
+  display: inline-block;
+`;
+
+export { Card };
