5a018a106f197ba4808de58431a8f1b9be147409
DESKTOP-FLKJGT3\DELL <naveen.atukorala@pyxeda.ai>
2025-01-10 11:36:14 +1030

    update(add return types for grid component, support for unit tests)

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -9,24 +9,24 @@ interface PropsType {
 }
 
 // Memoize the Grid component, so that it only re-renders if the props change
-export const Grid: React.FC<PropsType> = React.memo(({ gridState }) => {
+export const Grid: React.FC<PropsType> = React.memo(({ gridState }): React.JSX.Element => {
   const { gridSize, robots } = gridState;
 
-  const getColumnCells = useCallback((rowNumber: number): JSX.Element[] => {
-      // create a array map using the gridSize to optimize the perfomance of the cell generation functions
-      return Array.from({ length: gridSize }, (_, cellNumber) => {
-          const isRobotAtCell = robots.find((robot => robot.xCord === cellNumber && robot.yCord === rowNumber && robot.face && robot.isRobotPlaced))
-          if (isRobotAtCell) {
-            return <GridCellActive face={isRobotAtCell.face} key={`col${rowNumber}${cellNumber}`}>
-            <RobotId>{isRobotAtCell.id}</RobotId> </GridCellActive>;
-          }
-        return <GridCell key={`col${rowNumber}${cellNumber}`} />;
-      });
+  const getColumnCells = useCallback((rowNumber: number): React.JSX.Element[] => {
+    // create a array map using the gridSize to optimize the perfomance of the cell generation functions
+    return Array.from({ length: gridSize }, (_, cellNumber) => {
+      const isRobotAtCell = robots.find((robot => robot.xCord === cellNumber && robot.yCord === rowNumber && robot.face && robot.isRobotPlaced));
+      if (isRobotAtCell) {
+        return <GridCellActive face={isRobotAtCell.face} key={`col${rowNumber}${cellNumber}`} aria-label={'active-cell'}>
+          <RobotId>{isRobotAtCell.id}</RobotId> </GridCellActive>;
+      }
+      return <GridCell key={`col${rowNumber}${cellNumber}`} aria-label={`empty-cell`} />;
+    });
   }, [robots, gridSize]);
-  
+
   // Memoize this function, so it will compute the rows only if,
   // the values in dependency array changes
-  const generateGridRows = useMemo(() => {
+  const generateGridRows: React.JSX.Element[] = useMemo(() => {
     // create a array map using the gridSize to optimize the perfomance of the row generation functions
     return Array.from({ length: gridSize }, (_, rowNumber) => {
       const currentRow = gridSize - 1 - rowNumber;
