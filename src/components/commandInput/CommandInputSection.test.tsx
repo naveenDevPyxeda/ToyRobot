6d8c1dc319536cf9fb1e7e5b2c22be7f8631ac2c
DESKTOP-FLKJGT3\DELL <naveen.atukorala@pyxeda.ai>
2025-01-10 11:32:41 +1030

    update(add/update more unit tests to components, fix react import order)

--- src/components/commandInput/CommandInputSection.test.tsx
+++ src/components/commandInput/CommandInputSection.test.tsx
@@ -1,74 +1,72 @@
 
 import { render, screen, fireEvent } from '@testing-library/react';
+
 import { CommandInputSection } from './CommandInputSection';
 
 // Mock the dispatch function
 const mockOnEnter = jest.fn();
 
 describe('CommandInputSection', () => {
-  beforeEach(() => {
-    mockOnEnter.mockClear();
-  });
-
-  // Check if the input field and button are rendered
-  it('renders the input field and enter button correctly', () => {
-    render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
-
-    const inputElement = screen.getByLabelText('command-input');
-    const buttonElement = screen.getByRole('button', { name: /enter/i });
-
-    expect(inputElement).toBeInTheDocument();
-    expect(buttonElement).toBeInTheDocument();
-  });
-
-  // Make sure user input is correctly passed to the dispatch function
-  it('handles user input correctly and calls onEnter with the parsed command', () => {
-    render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
-
-    const inputElement = screen.getByLabelText('command-input');
-    const buttonElement = screen.getByRole('button', { name: /enter/i });
-    fireEvent.change(inputElement, { target: { value: 'MOVE 1 5 10 NORTH' } });
-    fireEvent.click(buttonElement);
-
-    expect(mockOnEnter).toHaveBeenCalledTimes(1);
-    expect(mockOnEnter).toHaveBeenCalledWith({
-      baseCommand: 'MOVE',
-      error: null,
-      face: 'NORTH',
-      xCord: 5,
-      yCord: 10,
-      id: 1,
+    beforeEach(() => {
+        mockOnEnter.mockClear();
     });
-  });
-  
-  // Check for input field, after enter
-  it('clears the input field after clicking Enter', () => {
-    render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
 
-    const inputElement = screen.getByLabelText('command-input');
-    const buttonElement = screen.getByRole('button', { name: /enter/i });
+    // Check if the input field and enter button are rendered
+    it('renders the input field and enter button correctly', () => {
+        render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
 
-    fireEvent.change(inputElement, { target: { value: 'MOVE 1 5 10 NORTH' } });
-    fireEvent.click(buttonElement);
+        const inputElement = screen.getByLabelText('command-input');
+        const buttonElement = screen.getByRole('button', { name: /enter/i });
+        expect(inputElement).toBeInTheDocument();
+        expect(buttonElement).toBeInTheDocument();
+    });
 
-    // Verify that the input field is cleared
-    expect(inputElement).toHaveValue('');
-  });
+    // Make sure user input is correctly passed to the dispatch function
+    it('handles user input correctly and calls onEnter with the parsed command', () => {
+        render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
 
-  // Check whether the error message is shown correctly
-  it('displays the error message if error prop is passed', () => {
-    render(<CommandInputSection error="Invalid command" onEnter={mockOnEnter} />);
-    const errorMessage = screen.getByLabelText('command-error');
-    expect(errorMessage).toBeInTheDocument();
-    expect(errorMessage).toHaveTextContent('Invalid command');
-  });
+        const inputElement = screen.getByLabelText('command-input');
+        const buttonElement = screen.getByRole('button', { name: /enter/i });
+        fireEvent.change(inputElement, { target: { value: 'MOVE 1 5 10 NORTH' } });
+        fireEvent.click(buttonElement);
+        expect(mockOnEnter).toHaveBeenCalledTimes(1);
+        expect(mockOnEnter).toHaveBeenCalledWith({
+            baseCommand: 'MOVE',
+            error: null,
+            face: 'NORTH',
+            xCord: 5,
+            yCord: 10,
+            id: 1,
+        });
+    });
 
-  // Check for the error component when there is no error
-  it('does not display error when error prop is null', () => {
-    render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
+    // Check for input field, after enter
+    it('clears the input field after clicking Enter', () => {
+        render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
 
-    // Check that the error message is not in the document
-    const errorMessage = screen.queryByLabelText('command-error');
-    expect(errorMessage).toBeNull();
-  });
+        const inputElement = screen.getByLabelText('command-input');
+        const buttonElement = screen.getByRole('button', { name: /enter/i });
+        fireEvent.change(inputElement, { target: { value: 'MOVE 1 5 10 NORTH' } });
+        fireEvent.click(buttonElement);
+        // Verify that the input field is cleared
+        expect(inputElement).toHaveValue('');
+    });
+
+    // Check whether the error message is shown correctly
+    it('displays the error message if error prop is passed', () => {
+        render(<CommandInputSection error="Invalid command" onEnter={mockOnEnter} />);
+
+        const errorMessage = screen.getByLabelText('command-error');
+        expect(errorMessage).toBeInTheDocument();
+        expect(errorMessage).toHaveTextContent('Invalid command');
+    });
+
+    // Check for the error component when there is no error
+    it('does not display error when error prop is null', () => {
+        render(<CommandInputSection error={null} onEnter={mockOnEnter} />);
+
+        // Check that the error message is not in the document
+        const errorMessage = screen.queryByLabelText('command-error');
+        expect(errorMessage).toBeNull();
+    });
 });
--- src/components/grid/Grid.test.tsx
+++ src/components/grid/Grid.test.tsx
@@ -1,23 +1,86 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
+
+import { DirectionofRobotFace } from '../../types/global-types';
 
 import { Grid } from './Grid';
-import { GridStateType } from '../../types/global-types';
 
-afterEach(() => {
-    cleanup();
-})
+const mockGridState = {
+    gridSize: 5,
+    robots: [
+        {
+            id: '1',
+            xCord: 2,
+            yCord: 3,
+            face: DirectionofRobotFace.North,
+            isRobotPlaced: true,
+        },
+        {
+            id: '2',
+            xCord: 4,
+            yCord: 1,
+            face: DirectionofRobotFace.East,
+            isRobotPlaced: true,
+        },
+    ],
+    error: null,
+};
 
-describe('Grid component', () => {
+describe('Grid Component', () => {
     // Check whether the grid has been rendered correctly
     it('renders the grid correctly', () => {
-        const mockGridState: GridStateType = {
-            gridSize: 5,
-            robots: [],
-            error: null,
-        }
+        render(<Grid gridState={mockGridState} />);
 
-        render(<Grid gridState={mockGridState}/>);
-        const allGridCells = screen.getAllByLabelText('empty-cell');
-        expect(allGridCells.length).toBe(25);
-    })
-})
+        const emptyGridCells = screen.getAllByLabelText('empty-cell');
+        const activeGridCells = screen.getAllByLabelText('active-cell');
+        expect(emptyGridCells.length).toBe(23);
+        expect(activeGridCells.length).toBe(2);
+    });
+
+    // Check for rows and columns
+    it('should render the correct number of rows and columns based on gridSize', () => {
+        render(<Grid gridState={mockGridState} />);
+
+        const rows = screen.getAllByRole('row');
+        expect(rows).toHaveLength(mockGridState.gridSize);
+        rows.forEach(row => {
+            const cells = within(row).getAllByRole('cell');
+            expect(cells).toHaveLength(mockGridState.gridSize);
+        });
+    });
+
+    // Check for dynamic change of the grid based on the gridsize
+    it('should render grid dynamically based on gridSize', () => {
+        const customGridState = { ...mockGridState, gridSize: 3 };
+
+        render(<Grid gridState={customGridState} />);
+
+        const rows = screen.getAllByRole('row');
+        expect(rows).toHaveLength(customGridState.gridSize);
+        rows.forEach(row => {
+            const cells = within(row).getAllByRole('cell');
+            expect(cells).toHaveLength(customGridState.gridSize);
+        });
+    });
+
+    // Check for a robot which is not placed in the grid
+    it('should not render robots if they are not placed', () => {
+        const unplacedRobotState = {
+            gridSize: 5,
+            robots: [
+                {
+                    id: '1',
+                    xCord: 2,
+                    yCord: 3,
+                    face: DirectionofRobotFace.East,
+                    isRobotPlaced: false, // Robot is not placed in this test case
+                },
+            ],
+            error: null,
+        };
+
+        render(<Grid gridState={unplacedRobotState} />);
+
+        const robots = screen.queryByText('1');
+        expect(robots).toBeNull();
+    });
+});
